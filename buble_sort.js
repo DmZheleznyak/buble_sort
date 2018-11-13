8d1411da708c2021ed6a4bc6e087c7f0b27bc6e0
Dmitriy <11lione1991@gmail.com>
2018-11-13 18:39:23 +0200

    delete check

--- buble_sort.js
+++ buble_sort.js
@@ -1,9 +1,7 @@
-var arr = [ 5,3242,3532,45,435,234, 2, 7, 34, 4, 6, 8]
+var arr = [ 500,430, 60, 8]
 
-const bubleSort = arr => {
-//   
-  var sorted = false
-// 
+const bubleSort = arr => { 
+
   var iterationOverAll = arr.length - 1
 
   for ( let i = 0; i < iterationOverAll; i++ ) {
@@ -11,19 +9,17 @@ const bubleSort = arr => {
     var iterationElement = iterationOverAll - i
 
     for ( let j = 0; j < iterationElement; j++ ) {
-
       if ( arr[j] > arr[j+1] ) {
-        [ arr[j], arr[j+1] ] = [ arr[j+1], arr[j] ]
+        [ arr[j], arr[j+1] ] = [ arr[j+1], arr[j] ]  
       } 
-
-      sorted = true
-
+      console.log(arr)
     }
 
-    if (!sorted) break
   }
 
   return arr
 }
 const sortedArr = bubleSort( arr )
-console.log( sortedArr )
+console.log(`sortedArr -`, sortedArr )
+
+// 
