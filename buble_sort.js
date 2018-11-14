125e1bae840864d7dc539d08afa11cdb19c3f5a2
Dmitriy <11lione1991@gmail.com>
2018-11-14 03:30:34 +0200

    add newArray

--- buble_sort.js
+++ buble_sort.js
@@ -1,25 +1,28 @@
-var arr = [ 500,430, 60, 8]
+const newArray = [ 27, 234, 5436, 74, 65, 1 ]
 
 const bubleSort = arr => { 
-
-  var iterationOverAll = arr.length - 1
-
+	
+	const iterationOverAll = arr.length - 1
+	
   for ( let i = 0; i < iterationOverAll; i++ ) {
-
-    var iterationElement = iterationOverAll - i
-
+ 
+    const iterationElement = iterationOverAll - i
+ 
     for ( let j = 0; j < iterationElement; j++ ) {
+			
       if ( arr[j] > arr[j+1] ) {
-        [ arr[j], arr[j+1] ] = [ arr[j+1], arr[j] ]  
+
+				[ arr[j], arr[j+1] ] = [ arr[j+1], arr[j] ]  
+				
       } 
-      console.log(arr)
+      
     }
 
   }
 
   return arr
 }
-const sortedArr = bubleSort( arr )
-console.log(`sortedArr -`, sortedArr )
 
-// 
+const sortedArr = bubleSort( newArray )
+
+console.log(`sortedArr -`, sortedArr ) 
