f430a115199a37bd12367dccbcf3908b63e8c247
Dmitriy <11lione1991@gmail.com>
2018-11-15 01:55:19 +0200

    add one step, not finish

--- quick_sort.js
+++ quick_sort.js
@@ -1,26 +1,36 @@
-const quickSorte = arr => {
+const quickSort = arr => {
 
-	const [...copyArray] = arr
+	const [...array] = arr
 
-	const pivot = copyArray => {
-		
-		if ( 5 < copyArray.length ) {
+	const middleIndex = Math.round( ( copyArray.length - 1 ) / 2 )
+	const pivot = array[ middleIndex ]	
+	let indexFirst = 0
+	let indexLast = array.length - 1 
 
-			const middleNumber = Math.round( copyArray.length / 2 )
+	while( indexFirst < indexLast ) {
 
-			return copyArray[middleNumber]
-
+		// var rightSideNumber = 0
+		console.log( array[middleIndex] )
+		while ( indexFirst < middleIndex  && array[ indexFirst ] < pivot ) {
+			indexFirst += 1
+		}
+		while ( indexLast > middleIndex && array[ indexLast ] > pivot ) {
+			indexLast -= 1
 		} 
 
-		console.log( pivot(copyArray) )
-
-
+		if ( indexFirst < indexLast ) {
+			[ array[indexFirst], array[indexLast] ] = [ array[indexLast], array[indexFirst] ]
+		}
 
 	} 
 
+	console.log(`pivot`, pivot ) // 354
+
 }
 
-const checkArray // to be continue
+const checkArray = [ 30, 25, 1000, 354, 60234, 750, 100, 234, 4, 45 ] 
+
+quickSort( checkArray ) 
 
 // Из-за рекурсии и других "накладных расходов" 
 // Quicksort может оказаться не столь уж быстрой для коротких массивов. 
